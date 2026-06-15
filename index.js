54043af90b03235c39d72496d2fe53db4f4af2d4
Alex <74328226+aliannoi@users.noreply.github.com>
2026-06-15 11:52:48 +0200

    update cv link

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const rootLink     = "#/";
 const githubLink   = "https://github.com/aliannoi";
 const linkedinLink = "https://linkedin.com/in/oleksandr-liannoi-a3410820a";
 const gmailLink    = "mailto:lyannoy.alexander@gmail.com";
-const cvLink       = "https://docs.google.com/document/d/1Spg0RcEWuvFPyAnHpTyA2r5PZ3Nue4BoQU56nbx_tPQ/export?format=pdf";
+const cvLink       = "https://docs.google.com/document/d/1ls1dzDjHjdCEG5Qj-uDLoHNn6BDknf1W26QWnPJjM1U/export?format=pdf";
 
 const rootSvg = domx.svg({ fill: "currentColor", width: "800px", height: "800px", viewBox: "0 0 492.5 492.5"},
                          domx.g({}, domx.path({ d: "M184.646,0v21.72H99.704v433.358h31.403V53.123h53.539V492.5l208.15-37.422v-61.235V37.5L184.646,0z M222.938,263.129 c-6.997,0-12.67-7.381-12.67-16.486c0-9.104,5.673-16.485,12.67-16.485s12.67,7.381,12.67,16.485 C235.608,255.748,229.935,263.129,222.938,263.129z" })));
